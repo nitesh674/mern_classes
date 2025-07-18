a2ac9848c333c3fb8adf28a7e1a9adf44fe1683c
nitesh Varma <kurmaverma391@gmail.com>
2025-07-18 01:53:48 -0600

    all file update "18-7-25"

---
+++ Register_form/script.js
@@ -0,0 +1,7 @@
+  gsap.from("#container",{
+    // y: -100,
+    scale:0.5,
+    opacity: 0,
+    duration: 1.2,
+    ease: "power3.out"
+  });
--- Simple_from/script.js
+++ Simple_from/script.js
@@ -1,6 +1,22 @@
-  gsap.to(".container", {
-    duration: 1.5,
-    opacity: 1,
-    scale: 1,
-    ease: "bounce.out",
+  // gsap.to(".container", {
+  //   duration: 1.5,
+  //   opacity: 1,
+  //   scale: 1,
+  //   ease: "bounce.out",
+  // });
+
+  // Animate container in from opacity 0 to 1
+  gsap.from(".container", {
+    duration: 2,
+    opacity: 0,
+    scale: 0.8,
+    ease: "power2.out"
   });
+  gsap.from(".backimg", {
+    x: -200,
+    opacity: 0,
+    duration: 1,
+    delay: 0.2,
+    ease: "back.out(1.4)"
+  });
+
