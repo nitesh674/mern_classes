6b002fc550637a47099c2f336f6fbad3b4a660c8
nitesh674 <kurmaverma391@gmail.com>
2025-08-13 21:17:42 -0600

    this is add Day - 17 and other code

--- javascript/Day-15/style.js
+++ javascript/Day-15/style.js
@@ -1,2 +1,14 @@
 
-con
+async function abc(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("my promise is resolve");
+        },5000);
+    })
+}
+
+
+
+async function fatchData(){
+    
+}
---
+++ javascript/Day-17/script.js
@@ -0,0 +1,41 @@
+
+
+// Exicution Context
+
+
+
+// let a = 12 ; // alocatied the memory 
+// console.log(a);
+
+
+
+// console.log(a);
+// let a;  // connt be access a befor initilaztion
+// const a; // undefined
+// var a; // undefined
+
+
+//scope 
+
+// function Scope 
+
+function xyz(){
+    let a = 12;
+}
+xyz();
+
+
+
+
+const a = [12334];
+
+
+let x = [12345] // 
+let z = [45677] // 
+console.log(x === z);
+function abc(){
+    // a = "and";
+    a.push(1233);
+}
+abc();
+console.log(a);
